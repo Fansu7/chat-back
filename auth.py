aec98c40bbc8cd1262d1019848fcca2e840e3297
Alfonso Vidal <vidaltorres.alfonso@gmail.com>
2025-10-09 10:37:28 +0200

    Update password verification to support bcrypt_sha256 hashing scheme

--- auth.py
+++ auth.py
@@ -19,7 +19,7 @@ def get_user_by_username(db: Session, username: str):
 
 
 def verify_password(plain_password, hashed_password):
-    return CryptContext(schemes=["bcrypt"], deprecated="auto").verify(plain_password, hashed_password)
+    return CryptContext(schemes=["bcrypt_sha256", "bcrypt"], deprecated="auto").verify(plain_password, hashed_password)
 
 def authenticate_user(db: Session, username: str, password: str):
     user = get_user_by_username(db, username)
