154be5de9522592c64d34e27b4e19b6b8e1b7853
Alfonso Vidal <vidaltorres.alfonso@gmail.com>
2025-10-14 11:14:34 +0200

    Revert "Update CORS middleware to use ALLOWED_ORIGINS for better security"
    This reverts commit 324ae2cfb961bc14547b03d43e5beeb371dcc736.

--- main.py
+++ main.py
@@ -13,14 +13,9 @@ app = FastAPI()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 origin = ["http://localhost:4200"]
 
-ALLOWED_ORIGINS = [
-    "https://fansu7.github.io",
-    "http://localhost:4200",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS,
+    allow_origins=origin,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
